ba3d6a258f6689afd15500568f44dfabd2984efa
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2024-07-29 18:29:49 +0700

    ✨ Add book reporting.

---
+++ main.py
@@ -0,0 +1,73 @@
+from collections import defaultdict
+
+def count_words(document: str) -> int:
+    """Return the number of words in the document.
+    
+    Args:
+        document: A string.
+    
+    Returns:
+        An integer.
+    """
+    return len(document.split())
+
+def count_chars(document: str) -> dict[str, int]:
+    """Return the number of chars
+      in the document.
+    
+    Args:
+        document: A string.
+    
+    Returns:
+        A dict mapping character to its occurences.
+    """
+    words = document.lower().split()
+    result = defaultdict(lambda: 0)
+                         
+    for word in words:
+        for c in word:
+            result[c] +=1
+
+    return dict(result)
+
+def sort_chars(chars_dict: dict[str, int]) -> list[dict]:
+    """Sort chars statistics by descending order.
+
+    Args:
+        chars_dict: A dict mapping character to its occurences.
+    
+    Returns:
+        A list of dict with the following structure:
+            { "name": "a", "count": 123 }
+    """
+    new_list = [{"name": k, "count": v} for k, v in chars_dict.items()]
+    return sorted(new_list, key=lambda x: x["count"], reverse=True)
+
+def report(filepath: str) -> str:
+    """Build a report about the book.
+
+    Args:
+        filepath: A file path to book.
+    
+    Returns:
+        A text report.
+    """
+
+    try:
+        with open(filepath) as f:
+            file_content = f.read()
+    except IOError:
+        return "Cannot create report: Wrong file path !"
+
+    text = f"--- Begin report of {filepath} ---\n"
+    text += f"{count_words(file_content)} words found in the document\n\n"
+    for item in sort_chars(count_chars(file_content)):
+        if item["name"].isalpha():
+            text += (
+                f"""The '{item["name"]}' character was found {item["count"]} times\n"""
+            )
+    return text
+
+def main():
+    print(report("books/frankenstein.txt"))
+main()
